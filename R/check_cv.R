195c9d44e98650f8490604d5973d9ebc77ba802a
westhues <matthias.westhues@uni-hohenheim.de>
2016-07-31 14:17:20 +0200

    Export check_cv function

--- R/check_cv.R
+++ R/check_cv.R
@@ -1,14 +1,23 @@
 #' Check TRN Assembly
-#' @keywords internal
+#'
+#' \code{check_cv} Check whether all CV runs fullfil requirements.
+#'
 #' @param x A matrix containing cross-validation folds inside columns and
 #'  hybrid names in rows.
 #' @param hybrid_split Character that separates parents in hybrid names.
+#'
 #' @return 'success', if all checks were successful.
+#'
+#' @examples
+#' data(hybrid_nms)
+#' check_cv(sample_cv(hybrid_nms, n_mother = 39, n_father = 33, n_trn = 200,
+#'                     min_size = 25, rounds = 100, hybrid_split = "_"))
+#' @export
 check_cv <- function(x, hybrid_split = "_") {
   fl_nm <- names(x)
-  trn_pos <- regexpr("(?<=_trn=)[0-9]+(?=_min_size=)", text = fl_nm, 
+  trn_pos <- regexpr("(?<=_trn=)[0-9]+(?=_min_size=)", text = fl_nm,
                      perl = TRUE)
-  trn_nm <- substring(fl_nm, first = trn_pos, 
+  trn_nm <- substring(fl_nm, first = trn_pos,
                       last = trn_pos + attr(trn_pos, "match.length") - 1)
   trn_nm <- as.numeric(trn_nm)
   min_pos <- regexpr("(?<=_min_size=)[0-9]+(?=_m=)", text = fl_nm, perl = TRUE)
@@ -16,11 +25,11 @@ check_cv <- function(x, hybrid_split = "_") {
                       last = min_pos + attr(min_pos, "match.length") - 1)
   min_nm <- as.numeric(min_nm)
   m_pos <- regexpr("(?<=_m=)[0-9]+(?=_f=)", text = fl_nm, perl = TRUE)
-  m_nm <- substring(fl_nm, first = m_pos, 
+  m_nm <- substring(fl_nm, first = m_pos,
                     last = m_pos + attr(m_pos, "match.length") - 1)
   m_nm <- as.numeric(m_nm)
   f_pos <- regexpr("(?<=_f=)[0-9]{2}", text = fl_nm, perl = TRUE)
-  f_nm <- substring(fl_nm, first = f_pos, 
+  f_nm <- substring(fl_nm, first = f_pos,
                     last = f_pos + attr(f_pos, "match.length") - 1)
   f_nm <- as.numeric(f_nm)
   dat <- x[[1]]
@@ -40,12 +49,12 @@ check_cv <- function(x, hybrid_split = "_") {
   if (!all(colSums(dat == "T2") >= min_nm)) {
     stop("T2 size too small")
   }
-  
+
   trn_parents <- vapply(seq_len(ncol(dat)), FUN = function(k) {
     trn_hyb <- rownames(dat)[dat[, k] == "TRN"]
-    mother_par <- vapply(strsplit(trn_hyb, split = hybrid_split), 
+    mother_par <- vapply(strsplit(trn_hyb, split = hybrid_split),
                          FUN = "[[", 2, FUN.VALUE = character(1))
-    father_par <- vapply(strsplit(trn_hyb, split = hybrid_split), 
+    father_par <- vapply(strsplit(trn_hyb, split = hybrid_split),
                          FUN = "[[", 3, FUN.VALUE = character(1))
     all(c(all.equal(length(unique(mother_par)), m_nm),
           all.equal(length(unique(father_par)), f_nm)))
--- tests/testthat/test_sample_cv.R
+++ tests/testthat/test_sample_cv.R
@@ -2,10 +2,10 @@ data("hybrid_nms")
 # -----------------------------------------------------------------------------
 context("Uniqueness of CV runs")
 test_that("no CV runs is duplicated", {
-  expect_identical(check_cv(sample_cv(hybrid_nms, n_mother = 39,
+  expect_identical(check_cv(sample_cv(hybrid_nms, n_mother = 40,
                                       n_father = 33,
                                       n_trn = 200,
-                                      min_size = 20, rounds = 20L,
+                                      min_size = 25, rounds = 1000,
                                       hybrid_split = "_")),
                    "success")
 })

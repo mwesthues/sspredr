542bfa80a8157f7de79e2726d2df10efb31cb36f
westhues <matthias.westhues@uni-hohenheim.de>
2017-02-08 14:35:43 +0100

    Simplify minor allele frequency function

--- R/maf.R
+++ R/maf.R
@@ -1,166 +1,177 @@
 #' Minor Allele Frequency
 #'
-#' \code{compute_maf} returns results from minor allele frequency check
+#' \code{compute_maf} returns results from minor allele frequency checks
 #'
 #' @param x A matrix. Genotype names are stored in rows whereas marker names
 #'  are stored in columns.
 #' @param output Character vector with output options.
 #' @param missing Character vector providing the encoding of missing elements.
-#' @param mafThresh Numeric or complex vector specifying the minor allele
+#' @param maf_threshold Numeric or complex vector specifying the minor allele
 #'  frequency threshold.
-#' @return If \code{output} is "markerNames" a character vector with marker
+#' @return If \code{output} is "marker_names" a character vector with marker
 #'  names that have passed the quality check will be returned. If \code{output}
-#'  is "markerMAF" a numeric vector with the minor allele frequency at each
-#'  marker locus will be returned. If \code{output} is "genoList" a list of two
-#'  elements will be returned, which provides the minor allele at each locus as
-#'  well as the major allele. The speed of computations is considerably higher
-#'  if the elements of \code{x} are supplied as character values since an
-#'  internal class conversion will be applied otherwise.
+#'  is "marker_maf" a numeric vector with the minor allele frequency at each
+#'  marker locus will be returned. If \code{output} is "recoded" a matrix with
+#'  the original dimensions will be returned where the most frequent genotype at
+#'  each locus will be encoded as the highest value and the least frequent
+#'  genotype will be encoded as the smalles value in \code{x}.
 #' @examples
 #'  # Load a matrix with SNP genotypes encoded as numeric values
 #'  data(marker_numeric)
 #'
 #'  # Return the names of all polymorphic markers.
-#'  compute_maf(marker_numeric, output = "markerNames", missing = NA_real_,
+#'  compute_maf(marker_numeric, output = "marker_names", missing = NA_real_,
 #'              mafThresh = 0)
 #'
 #'  # Set one locus to monomorphic
 #'  marker_mono <- marker_numeric
 #'  marker_mono[, 1] <- 1
-#'  compute_maf(marker_numeric, output = "markerNames", missing = NA_real_,
+#'  compute_maf(marker_numeric, output = "marker_names", missing = NA_real_,
 #'              mafThresh = 0)
 #'
 #'  # Load a matrix with SNP genotypes encoded as character values
 #'  data(marker_character)
 #'
 #'  # Return the minor allele frequency at each locus with MAF >= 0.05.
-#'  compute_maf(marker_character, output = "markerMAF", missing = "??",
+#'  compute_maf(marker_character, output = "marker_maf", missing = "??",
 #'              mafThresh = 0.05)
 #'
 #'  # Return the genotype of the major as well as of the minor allele at each
 #'  # locus.
-#'  compute_maf(marker_character, output = "genoList", missing = "??",
+#'  compute_maf(marker_character, output = "recoded", missing = "??",
 #'              mafThresh = 0)
 #' @export
-compute_maf <- function(x, output = c("markerNames", "markerMAF", "genoList"),
-                        missing = "??", mafThresh = 0) {
+compute_maf <- function(
+  x,
+  output = c("marker_names", "marker_maf", "recoded"),
+  missing,
+  maf_threshold) {
+
+
+  # Store the original type of the input so that results for the
+  # 'output'-option 'recoded' can be converted back to the original type.
+  x_type <- typeof(x)
+
+  # General input checks
   if (class(x) != "matrix") {
     stop("Input has to be of class 'matrix'")
   }
-  if (class(missing) != class(c(x))) {
-    stop("Classes of 'missing' and 'x' must be the same.")
+
+  if (typeof(missing) != typeof(x)) {
+    stop("Types of 'missing' and 'x' must be the same.")
   }
-  # Recode SNP markers to class 'character' if they are not yet incoded as such.
-  if (class(c(x)) != "character") {
-    put <- sort(stats::na.exclude(unique(c(x)[!c(x) %in% missing])))
-      storage.mode(x) <- "character"
-      x[x %in% missing] <- "??"
-      missing <- "??"
-    if (length(put) == 2) {
-      x[x == put[1]] <- "AA"
-      x[x == put[2]] <- "BB"
-    } else if (length(put) == 3) {
-      if (mean(put[c(1, 3)]) != put[2]) {
-        stop("Value for heterozygotes must be equal to the mean of homozygotes")
-      } else {
-        x[x == put[1]] <- "AA"
-        x[x == put[2]] <- "AB"
-        x[x == put[3]] <- "BB"
+
+  if (any(colMeans(is.na(x)) > 0.1)) {
+  warning("Some markers have more than 10% missing values")
+  }
+
+  unique_elements <- sort(na.exclude(unique(c(x))))
+
+  # Checks that are specific to numeric input matrices.
+  if (typeof(x) %in% c("double", "integer")) {
+    # Get the unique numeric elements in order to check whether the encoding of
+    # genotypes with respect to homozygous (recessive and dominant) and
+    # heterozygous genotypes makes sense.
+    unique_elements <- sort(na.exclude(unique(c(x))))
+    if (length(unique_elements) > 3) {
+      stop("Only recessive, dominant and heterozygous genotypes are allowed.")
+    }
+    if (length(unique_elements) < 2) {
+      stop("Only one unique genotype present")
+    }
+    if (length(unique_elements) == 3) {
+      if (!unique_elements[2] == mean(c(unique_elements[1], unique_elements[3]))) {
+        stop("Heterozygous value must be the mean of the homozygous values")
       }
     }
   }
-  # Names of all genotypes present in the marker data matrix.
-  allgeno <- stats::na.exclude(unique(c(x)))
 
-  if (!all(nchar(allgeno) == 2)) {
-    stop(cat("Alleles must be encoded by two letters."))
-  }
-  if (any(allgeno %in% c("XX", "XY", "YY"))) {
-    stop("Letters 'X' and 'Y' are reserved for output")
-  }
-  if (mafThresh >= 1) {
-    stop("A MAF threshold >= 1 will discard all loci.")
-  }
-  if (mafThresh < 0) {
-    stop("The MAF threshold has to be positiive.")
+
+  # It is assumed that the minor genotype in 'x' is encoded with a lower value
+  # than the major genotype and that the heterozygous genotype is encoded as
+  # the arithmetic mean of the two values.
+  if (length(unique_elements) == 3) {
+    min_value <- unique_elements[1]
+    het_value <- unique_elements[2]
+    max_value <- unique_elements[3]
+    n_het <- colSums(x == het_value, na.rm = TRUE)
+  } else {
+    min_value <- unique_elements[1]
+    max_value <- unique_elements[2]
   }
 
-  output <- match.arg(output)
-  # Separate heterozygous genotypes.
-  namesplit <- strsplit(allgeno, split = "")
-  heteroz <- allgeno[unlist(lapply(namesplit, function(x) x[1] != x[2]))]
-  # Separate homozygous genotypes.
-  homoz <- allgeno[!allgeno %in% c(missing, heteroz)]
-  # Matrix for storage of genotype frequencies.
-  genomatrix <- matrix(NA_real_, nrow = length(allgeno), ncol = ncol(x))
-  rownames(genomatrix) <- allgeno
-  colnames(genomatrix) <- colnames(x)
-  # Frequency matrix of genotypes by marker.
-  for (i in allgeno) {genomatrix[i, ] <- colSums(x == i)}
-  # Genotypes excluding missings ("??").
-  excna <- genomatrix[rownames(genomatrix) != missing, ]
-  # Remove all markers with merely missing values.
-  nona <- excna[, colSums(excna) != 0]
-  # Remove all monomorphic markers.
-  poly <- nona[, colSums(nona != 0) > 1]
-  # Reduce the dimensions of the original data frame so that only polymorphic
-  # markers are included.
-  polyX <- x[, colnames(x) %in% colnames(poly)]
-  # Reduce the genotype matrix to homozygous genotypes.
-  homozmat <- poly[homoz, ]
-  if (any(colSums(homozmat != 0) > 2)) stop(paste("markers with more than",
-                                                  "two homozygous genotypes",
-                                                  "present"))
-  # For each locus, select the major and the minor genotype.
-  n <- nrow(homozmat)
-  major <- vapply(X = seq_len(ncol(homozmat)), FUN = function(i) {
-    x <- homozmat[, i]
-    majorGeno <- names(x[x == sort(x, partial = n)[n]])
-    if(length(majorGeno) != 1) {
-      majorGeno <- majorGeno[1]
+  # Count the occurrences of each genotype to determine the minor allele
+  # frequency at each locus.
+  n_min <- colSums(x == min_value, na.rm = TRUE)
+  n_maj <- colSums(x == max_value, na.rm = TRUE)
+
+  if(typeof(x) == "character") {
+    # For each locus, check whether there are no more than two alleles.
+    x[x == missing] <- NA_character_
+    if (length(unique_elements) > 3) {
+      stop("Only recessive, dominant and heterozygous genotypes are allowed.")
     }
-    return(majorGeno)
-  }, FUN.VALUE = "character")
-
-  minor <- vapply(X = seq_len(ncol(homozmat)), FUN = function(i) {
-    x <- homozmat[,i]
-    minorGeno <- names(x[x == sort(x, partial = n - 1)[n - 1]])
-    if (length(minorGeno) != 1) {
-      minorGeno <- minorGeno[2]
+    if (length(unique_elements) < 2) {
+      stop("Only one unique genotype present")
     }
-    return(minorGeno)
-  }, FUN.VALUE = "character")
-  # For which loci are there no true major and minor genotypes?
-  stopifnot(all(major != minor))
+    if (length(unique_elements) == 3) {
+      dom_allele <- purrr::flatten_chr(strsplit(max_value, split = ""))[1]
+      rec_allele <- purrr::flatten_chr(strsplit(min_value, split = ""))[1]
+      split_het_value <- purrr::flatten_chr(strsplit(het_value, split = ""))
+      if (!dom_allele %in% split_het_value || !rec_allele %in% split_het_value) {
+        stop("Heterozygotes must be encoded by the unique letters of homozygotes")
+      }
+    }
+  }
 
-  majorMat <- matrix(major, nrow = 1)[rep(1, nrow(polyX)), ]
-  minorMat <- matrix(minor, nrow = 1)[rep(1, nrow(polyX)), ]
 
-  # Augment the matrix with reference/minor genotypes to have the same number
-  # of rows as the input (Dent or Flint). Replace genotypes by new
-  # identifiers for major, minor and heterozygous genotypes, respectively.
-  polyX[polyX == majorMat] <- "XX"
-  polyX[polyX == minorMat] <- "YY"
-  polyX[!polyX %in% c("XX", "YY", missing)] <- "XY"
-  n_maj <- colSums(polyX == "XX")
-  n_min <- colSums(polyX == "YY")
-  n_het <- colSums(polyX == "XY")
+  recoding_vector <- rep(min_value, times = ncol(x))
+  recoding_vector[n_maj >= n_min] <- max_value
+  storage.mode(recoding_vector) <- "character"
+  storage.mode(x) <- "character"
+
+  # For each locus, determine whether the assumed major genotype is actually
+  # the most frequent one. If this is not the case (if) recode the genotypes
+  # to match the expected values in 'unique_elements', otherwise (else) leave
+  # them as before.
+  recoded_x <- lapply(seq_len(ncol(x)), FUN = function(i) {
+    x_col <- x[, i]
+    actual_major <- recoding_vector[i]
+    if (actual_major != as.character(max_value)) {
+      x_col[x_col == actual_major] <- "major"
+      x_col[x_col == max_value] <- "minor"
+    } else {
+      x_col[x_col == min_value] <- "minor"
+      x_col[x_col == max_value] <- "major"
+    }
+    x_col
+  })
+  recoded_x <- matrix(unlist(recoded_x), ncol = ncol(x), byrow = FALSE)
+  recoded_x[recoded_x == "minor"] <- min_value
+  recoded_x[recoded_x == "major"] <- max_value
+  if (x_type %in% c("double", "integer")) {
+    storage.mode(recoded_x) <- "integer"
+  }
+
+  # Count the occurrences of each genotype to determine the minor allele
+  # frequency at each locus.
+  n_min <- colSums(recoded_x == min_value, na.rm = TRUE)
+  n_maj <- colSums(recoded_x == max_value, na.rm = TRUE)
+  if (length(unique_elements) == 3) {
+    n_het <- colSums(recoded_x == het_value, na.rm = TRUE)
+  } else {
+    n_het <- 0
+  }
   # Number of genotypes, which are not missing.
   n_geno <- n_maj + n_min + n_het
   # Frequency of the major allele at any locus.
   p_maj <- ((2 * n_maj) + n_het) / (2 * n_geno)
   # Frequency of the minor allele at any locus.
   p_min <- 1 - p_maj
-  stopifnot(max(p_min) <= 0.5)
 
-  if (mafThresh == 0) {
-    markernames <- colnames(polyX)
-  } else {
-    markernames <- colnames(polyX)[p_min >= mafThresh]
-  }
-
+  output <- match.arg(output)
   switch(output,
-         markerNames = markernames,
-         markerMAF = p_min,
-         genoList = list(major_allele = major, minor_allele = minor))
+         marker_names = colnames(x[, p_min >= maf_threshold]),
+         marker_maf = list(major = p_maj, minor = p_min),
+         recoded = recoded_x)
 }
--- tests/testthat/test_maf.R
+++ tests/testthat/test_maf.R
@@ -1,29 +1,47 @@
 context("MAF input")
 test_that("arguments play nice", {
-  expect_error(compute_maf(marker_numeric, output = "markerNames",
+  expect_error(compute_maf(marker_numeric, output = "marker_names",
                            missing = "??",
-                           mafThresh = 0),
-               regexp = "Classes of 'missing' and 'x' must be the same")
-  expect_error(compute_maf(marker_character, output = "markerNames",
-                         missing = NA_real_,
-                         mafThresh = 0),
-             regexp = "Classes of 'missing' and 'x' must be the same")
+                           maf_threshold = 0),
+               regexp = "Types of 'missing' and 'x' must be the same")
+  expect_error(compute_maf(marker_character, output = "marker_names",
+                           missing = NA_real_,
+                           maf_threshold = 0),
+             regexp = "Types of 'missing' and 'x' must be the same")
 })
 
-test_that("numeric and character values are supported", {
-  expect_identical(compute_maf(marker_numeric, output = "markerNames",
-                               missing = NA_real_,
-                               mafThresh = 0),
-                   compute_maf(marker_character, output = "markerNames",
-                               missing = "??",
-                               mafThresh = 0))
+
+context("MAF recoding")
+dummy_input <- matrix(
+  c(
+    2, 0, 0, 0, 2,
+    0, NA_real_, 2, 2, 2,
+    0, 0, 0, 2, 0
+  ),
+  ncol = 3
+)
+
+dummy_output <- matrix(
+  c(
+    0, 2, 2, 2, 0,
+    0, NA_real_, 2, 2, 2,
+    2, 2, 2, 0, 2
+  ),
+  ncol = 3
+)
+
+test_that("Major alles are defined correctly", {
+  expect_equal(compute_maf(dummy_input, output = "recoded",
+                           missing = NA_real_,
+                           maf_threshold = 0),
+               dummy_output)
 })
 
 context("MAF output")
 test_that("output structure is correct", {
-  expect_length(compute_maf(marker_numeric, output = "markerNames",
+  expect_length(compute_maf(marker_numeric, output = "marker_names",
                             missing = NA_real_,
-                            mafThresh = 0),
+                            maf_threshold = 0),
                 n = ncol(marker_numeric))
 })
 

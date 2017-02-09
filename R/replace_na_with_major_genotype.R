35339678fe1dc26fadc104f3cb966916827958af
westhues <matthias.westhues@uni-hohenheim.de>
2017-02-09 12:04:08 +0100

    Purge magrittr pipe from examples

--- R/replace_na_with_major_genotype.R
+++ R/replace_na_with_major_genotype.R
@@ -14,11 +14,9 @@
 #'  data(marker_numeric)
 #'
 #'  # Compute the major genotype at each locus.
-#'  major_genotype <- marker_numeric %>%
-#'    sspredr::compute_maf(output = "geno_list", missing = NA_real_,
-#'                         maf_threshold = 0) %>%
-#'    .["major"] %>%
-#'    purrr::flatten_int()
+#'  major_genotype <- sspredr::compute_maf(marker_numeric, output = "geno_list",
+#'                                         missing = NA_real_,
+#'                                         maf_threshold = 0)[["major"]]
 #'
 #'  # Replace all missing genotypes with the major allele.
 #'  replace_na_with_major_genotype(marker_numeric, missing_value = NA_real_,
